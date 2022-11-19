a359ec3b65eb930415863ee7442cb2095a6d4a1b
Battleworriorho <117505835+ZProtons@users.noreply.github.com>
2022-11-19 13:01:54 +0530

    update

--- java.js
+++ java.js
@@ -2,7 +2,7 @@ import kaboom from "https://unpkg.com/kaboom/dist/kaboom.mjs";
 kaboom();
 
 // load a sprite "bean" from an image
-loadSprite("dino", "sprites/dine.jped")
+loadSprite("dino", "sprites/dino.jpeg")
 
 // add something to screen
 add([
